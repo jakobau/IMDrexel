23cdec094e2014ebbc463b3afe2ff938adcd2c64
Matt D'Ulisse <mattd0312@gmail.com>
2020-03-10 01:17:05 -0400

    added barebones server.js file

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const server = express();
+const mysql = require("mysql");
+
+server.listen(8080, function() { console.log("Server open on 8080...") })
+
+const connection = mysql.createConnection({
+  host     : process.env.RDS_HOSTNAME,
+  user     : process.env.RDS_USERNAME,
+  password : process.env.RDS_PASSWORD,
+  port     : process.env.RDS_PORT
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('Database connection failed: ' + err.stack);
+    return;
+  }
+
+  console.log('Connected to database.');
+});
+
+connection.end();
+
+app.get("/dashboard", (req, res) => {
+    return//TODO
+}
