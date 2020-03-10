0aaa53763394e622b8b7652e39d320c2be1b0bff
Matt D'Ulisse <mattd0312@gmail.com>
2020-03-10 02:02:58 -0400

    make and send default object

--- server.js
+++ server.js
@@ -4,24 +4,36 @@ const mysql = require("mysql");
 
 server.listen(8080, function() { console.log("Server open on 8080...") })
 
-const connection = mysql.createConnection({
-  host     : process.env.RDS_HOSTNAME,
-  user     : process.env.RDS_USERNAME,
-  password : process.env.RDS_PASSWORD,
-  port     : process.env.RDS_PORT
-});
-
-connection.connect(function(err) {
-  if (err) {
-    console.error('Database connection failed: ' + err.stack);
-    return;
-  }
-
-  console.log('Connected to database.');
-});
-
-connection.end();
+//const connection = mysql.createConnection({
+//  host     : process.env.RDS_HOSTNAME,
+//  user     : process.env.RDS_USERNAME,
+//  password : process.env.RDS_PASSWORD,
+//  port     : process.env.RDS_PORT
+//});
+//
+//connection.connect(function(err) {
+//  if (err) {
+//    console.error('Database connection failed: ' + err.stack);
+//    return;
+//  }
+//
+//  console.log('Connected to database.');
+//});
+//
+//connection.end();
 
 app.get("/dashboard", (req, res) => {
-    return//TODO
+  var obj = {};
+  obj["sport"] = "Basketball";
+  obj["league"] = "5v5 Mens League";
+  obj["Time"] = "17:00";
+  obj["Day"] = "Tuesday";
+  obj["Date"] = "4 May 2020";
+  obj["Location"] = "@Court1";
+  obj["Opponent"] = "KILLERS";
+  obj["OpponentWins"] = "7";
+  obj["OpponentDraws"] = "0";
+  obj["OpponentLosses"] = "0";
+  
+  res.send(obj);
 }
