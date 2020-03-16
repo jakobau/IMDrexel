81531af23e1e7028eaca325d820d4cd58c4e771e
jakobau <30506552+jakobau@users.noreply.github.com>
2020-03-16 10:55:01 -1000

    update dashboards games and events

--- public/js/dashboard_events.js
+++ public/js/dashboard_events.js
@@ -1,14 +1,48 @@
 var events = document.getElementById('events');
 
-window.addEventListener("load", function() {
+game1.addEventListener("click", function() {
   var xhr = new XMLHttpRequest();
-  xhr.addEventListener("load", responseReceivedHandler);
-  xhr.responseType = "json";
+  xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      change(this);
+    }
+  };
   xhr.open('GET', '/dashboardevents', true);
   xhr.send();
-  events.innerHTML += "<h2>GAAAAME 5</h2>";
 });
 
-function responseReceivedHandler() {
-  return
+function change(xml) {
+  //xml response in JSON
+  var obj = JSON.parse(xml.response);
+
+  //building div for gameInfo
+  var div = document.createElement("div");
+
+  var content0 = document.createElement("h5");
+  content0.innerHTML = "<a href=\"#\">Drexel Basketball Mens @ 7PM Sun 2/20</a>";
+  div.append(title);
+
+  var content1 = document.createElement("h5");
+  content1.innerHTML = "<a href=\"#\">Drexel Basketball Womens @ 2PM Sat 3/13</a>";
+  div.append(content1);
+
+  var content2 = document.createElement("h5");
+  content2.innerHTML = "<a href=\"#\">Drexel Basketball Womens @ 2PM Sat 3/13</a>";
+  div.append(content2);
+
+  var content3 = document.createElement("h5");
+  content3.innerHTML = "<a href=\"#\">Squash Tourney @3/13 - 3/30</a>";
+  div.append(content3);
+
+  var content4 = document.createElement("h5");
+  content4.innerHTML = "<a href=\"#\">Drexel Volleyball Womens @ 8PM 3/15</a>";
+  div.append(content4);
+
+  var content5 = document.createElement("p");
+  content5.innerHTML ="<a href=\"#\">more info</a>";
+  div.append(content5);
+
+  //reset div and update
+  gameInfo.innerHTML = "";
+  gameInfo.append(div);
 }
--- public/js/dashboard_games.js
+++ public/js/dashboard_games.js
@@ -9,7 +9,7 @@ game1.addEventListener("click", function() {
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      change(this);
+      change(this, 1);
     }
   };
   xhr.open('GET', '/dashboardgames?gamenum=1', true);
@@ -20,7 +20,7 @@ game2.addEventListener("click", function() {
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      change(this);
+      change(this, 2);
     }
   };
   xhr.open('GET', '/dashboardgames?gamenum=2', true);
@@ -31,7 +31,7 @@ game3.addEventListener("click", function() {
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      change(this);
+      change(this, 3);
     }
   };
   xhr.open('GET', '/dashboardgames?gamenum=3', true);
@@ -42,7 +42,7 @@ game4.addEventListener("click", function() {
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      change(this);
+      change(this, 4);
     }
   };
   xhr.open('GET', '/dashboardgames?gamenum=4', true);
@@ -53,20 +53,23 @@ game5.addEventListener("click", function() {
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      change(this);
+      change(this, 5);
     }
   };
   xhr.open('GET', '/dashboardgames?gamenum=5', true);
   xhr.send();
 });
 
-function change(xml) {
+function change(xml, num) {
+  //game number and xml response in JSON
+  var gameNum = num;
   var obj = JSON.parse(xml.response);
 
+  //building div for gameInfo
   var div = document.createElement("div");
 
   var title = document.createElement("h3");
-  title.innerHTML = obj.sport + " " + obj.league;
+  title.innerHTML = "<a href=\"#\">" + obj.sport + " " + obj.league + "</a>";
   div.append(title);
 
   var content1 = document.createElement("h4");
@@ -74,17 +77,22 @@ function change(xml) {
   div.append(content1);
 
   var content2 = document.createElement("h4");
-  content2.innerHTML += obj.Location;
+  content2.innerHTML += "<a href=\"#\">" + obj.Location + "</a>";
   div.append(content2);
 
   var content3 = document.createElement("h4");
-  content3.innerHTML += "VS " + obj.Opponent;
+  content3.innerHTML += "VS " + "<a href=\"#\">" + obj.Opponent + "</a>";
   div.append(content3);
 
   var content4 = document.createElement("h5");
-  content4.innerHTML += obj.OpponentWins + " | " + obj.OpponentDraws + " | " + obj.OpponentLosses;
+  content4.innerHTML += "record: " + obj.OpponentWins + " | " + obj.OpponentDraws + " | " + obj.OpponentLosses;
   div.append(content4);
 
+  var content5 = document.createElement("p");
+  content5.innerHTML ="<a href=\"/gameinfo.html?myVar=09485\">more info</a>";
+  div.append(content5);
+
+  //reset div and update
   gameInfo.innerHTML = "";
   gameInfo.append(div);
 }
