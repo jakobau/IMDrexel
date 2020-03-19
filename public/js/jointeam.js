127b911dca37822ae525969cdc3ddb356bb6ebbf
jakobau <30506552+jakobau@users.noreply.github.com>
2020-03-18 16:20:29 -1000

    alert in jointeam

--- public/js/jointeam.js
+++ public/js/jointeam.js
@@ -6,3 +6,34 @@ var pastDiv1 = document.getElementById('pastDiv1');
 
 var pastLeague2 = document.getElementById('pastLeague2');
 var pastDiv2 = document.getElementById('pastDiv2');
+
+var alert = document.getElementById('alert');
+var button1 = document.getElementById('button1');
+
+button1.addEventListener('click', function() {
+  var div = document.createElement("div");
+  div.setAttribute("class", "alert alert-success alert-dismissible");
+  div.setAttribute("style","width:85%;");
+
+  var button = document.createElement("button");
+  button.setAttribute("type", "button");
+  button.setAttribute("class", "close");
+  button.setAttribute("data-dismiss", "alert");
+  button.innerHTML="&times;";
+
+  div.append(button);
+
+  var span = document.createElement("span");
+  span.innerHTML="<strong>Success!</strong> You have joined a team.";
+
+  div.append(span);
+
+  alert.append(div);
+  console.log("trying");
+});
+/*
+<div class="alert alert-success alert-dismissible" style="width:85%;">
+  <button type="button" class="close" data-dismiss="alert">&times;</button>
+  <strong>Success!</strong> Indicates a successful or positive action.
+</div>
+*/
